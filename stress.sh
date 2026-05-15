c20100969df4c0fd2f685c748d9f618076d94c32
iedo <iedoduarte1@gmail.com>
2026-05-15 09:25:59 -0300

    w

--- stress.sh
+++ stress.sh
@@ -2,7 +2,7 @@
 set -e
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-TEST_DIR="./challenge-guide/test"
+TEST_DIR="./spec/test"
 
 # docker buildx build --platform linux/amd64 -t rinha-2026:local .
 #
